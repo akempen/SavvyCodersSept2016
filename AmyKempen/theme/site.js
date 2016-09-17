7c3191d819f135fa800dc1434550233cfcbd8831
Amy Scott <amyscott@macintosh.attlocal.net>
2016-09-17 16:01:11 -0500

    Reorganize CSS

--- AmyKempen/theme/site.js
+++ AmyKempen/theme/site.js
@@ -1,12 +1,12 @@
-var favoriteCountry = prompt ( "What is your favorite country?" );
-
-var freedomCheck = function(){
-    if ( favoriteCountry === "USA" ){
-        console.log( "Land of Liberty");
-    } else {
-        console.log( "Awesome");
-    }
-
-};
-
-freedomCheck();
+// var favoriteCountry = prompt ( "What is your favorite country?" );
+//
+// var freedomCheck = function(){
+//     if ( favoriteCountry === "USA" ){
+//         console.log( "Land of Liberty");
+//     } else {
+//         console.log( "Awesome");
+//     }
+//
+// };
+//
+// freedomCheck();
