334b18099385fdf3542b6a885b56bd0991fba4f3
Amy Scott <amyscott@macintosh.attlocal.net>
2016-09-18 22:43:53 -0500

    Refactor pages and centered video

--- AmyKempen/theme/site.js
+++ AmyKempen/theme/site.js
@@ -1,12 +1,12 @@
-var favoriteCountry = prompt ( "What is your favorite country?" );
-
-var freedomCheck = function(){
-    if ( favoriteCountry === "USA" ){
-        console.log( "You need to travel more");
-    } else {
-        console.log( "Keep on travelin'");
-    }
-
-};
-
-freedomCheck();
+// var favoriteCountry = prompt ( "What is your favorite country?" );
+//
+// var freedomCheck = function(){
+//     if ( favoriteCountry === "USA" ){
+//         console.log( "You need to travel more");
+//     } else {
+//         console.log( "Keep on travelin'");
+//     }
+//
+// };
+//
+// freedomCheck();
