b7b378f40a7bb62d0ba38122131c184efc0263f6
Amy Scott <amyscott@Macintosh.local>
2016-09-21 14:57:15 -0500

    Make choose-your-own-adventure javascript doc work

--- AmyKempen/projects/choose-your-own-adventure/site.js
+++ AmyKempen/projects/choose-your-own-adventure/site.js
@@ -4,14 +4,13 @@ var ancilliaryCharacter = {}
 
 //Data
 var story = {
-    italy: {
-        italyCityPrompt: "Which city should we go to?",
-        italyCityOptions: {
-            yay: "Florence",
-            nay: "Rome"
-        }
-    },
-    mexico: "Boo, Italy is WAY better."
+        italy: {
+            florence: "You grab a cab and head to the Uffizi.",
+            florenceYes: "You meet two charming Italians, fall madly in love, move to Florence, and live happily-ever-after.",
+            florenceNo: "You nerd out on art all afternoon, pig out on pizza, get drunk off wine, and end the night with gelato.",
+            rome: "You realize Rome is in the middle of a zombie apocolypse so you run for dear life in search of other uninfected people."
+        },
+        mexico: "It's beach time so sit back and chillax with a margarita. You deserve it. The end."
 };
 
 //Helper functions
@@ -26,15 +25,35 @@ ancilliaryCharacter.name = prompt ( "What's your friend's name?" );
 console.log (mainCharacter);
 console.log (ancilliaryCharacter);
 
-promptText = prompt ( mainCharacter.name + " and " + ancilliaryCharacter.name + " decide to go on a trip together. Where should they go? Italy/Mexico");
+//Step 1
+promptText = prompt ( mainCharacter.name + " and " + ancilliaryCharacter.name + " decide to go on a trip together. Should you go to Italy or Mexico");
 
 if (promptText === "Italy") {
-    prompt (story.italy.italyCityPrompt);
+    promptText = prompt ( " Which city would you like to visit? Florence or Rome" );
 }
 
 else if (promptText === "Mexico") {
-    updateOutput (story.mexico);
+    updateOutput ( story.mexico );
+};
+
+//Step 2
+if (promptText === "Florence") {
+    promptText = prompt ( story.italy.florence + " Are you feeling adventurous, yes or no?" );
 }
+
+else if (promptText === "Rome") {
+    updateOutput ( story.italy.rome );
+};
+
+
+//Step 3
+if (promptText === "yes") {
+    updateOutput (story.italy.florenceYes);
+}
+
+else if (promptText === "no") {
+    updateOutput (story.italy.florenceNo);
+};
 
 
 
