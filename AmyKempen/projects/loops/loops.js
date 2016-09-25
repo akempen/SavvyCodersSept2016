fb69d827bdcda281b697dcdf8fc9f0d17d0a9f5f
Amy Scott <amyscott@macintosh.attlocal.net>
2016-09-25 07:39:16 -0500

    Working rock-paper-scissor game in javascript that puts in HTML

--- AmyKempen/projects/loops/loops.js
+++ AmyKempen/projects/loops/loops.js
@@ -1,130 +1,116 @@
-var userChoice = prompt ("Choose rock, paper, or scissor");
-    console.log (userChoice);
+//Rock-Paper-Scissor
 
-var computerChoice = Math.random (); {
-    if (computerChoice <= 0.3) {
-        console.log ("rock")
-    }
-    else if (computerChoice <= 0.6) {
-        console.log("scissor")
-    }
-    else {
-        console.log ("paper")
-    }
+var rockPaperScissor = function() {
+    var userOutputTarget = document.querySelector("#rock-paper-scissor-user");
+    var computerOutputTarget = document.querySelector("#rock-paper-scissor-computer");
+    var outputTarget = document.querySelector("#rock-paper-scissor");
+
+    var userOutputHtml = "";
+
+    var computerOutputHtml = "";
+
+    var outputHtml = "";
+
+    var userChoice = prompt ("Choose rock, paper, or scissor");
+        userOutputHtml += "<p>user: " + userChoice + "</p>";
+
+    var computerChoice = Math.random (); {
+        if (computerChoice <= 0.3) {
+            computerChoice = "rock";
+            userOutputHtml += "<p>computer: " + computerChoice + "</p>";
+        }
+        else if (computerChoice <= 0.6) {
+            computerChoice = "scissor";
+            computerOutputHtml += "<p>computer: " + computerChoice + "</p>";
+        }
+        else {
+            computerChoice = "paper";
+            computerOutputHtml += "<p>computer: " + computerChoice + "</p>";
+        }
+    };
+
+    var compare = function ( userChoice, computerChoice ) {
+        if ( userChoice === computerChoice ) {
+            outputHtml = "<p>It's a tie!</p>";
+        }
+        if ( ( userChoice === "rock" && computerChoice === "scissor" ) || ( userChoice === "scissor" && computerChoice === "rock" ) ) {
+            outputHtml = "<p>Rock crushes scissor, Rock wins!</p>";
+        }
+        else if ( ( userChoice === "scissor" && computerChoice === "paper" ) || ( userChoice === "paper" && computerChoice === "scissor" ) ) {
+            outputHtml = "<p>Scissor cuts paper, Scissor wins!</p>";
+        }
+        else if ( ( userChoice === "paper"  && computerChoice === "rock" ) || ( userChoice === "rock" && computerChoice === "paper" ) ) {
+            outputHtml = "<p>Paper covers rock. Paper wins!</p>";
+        }
+
+    };
+    compare ( userChoice, computerChoice );
+    userOutputTarget.innerHTML = userOutputHtml;
+    computerOutputTarget.innerHTML = computerOutputHtml;
+    outputTarget.innerHTML = outputHtml;
+
+};
+rockPaperScissor ();
+
+
+
+//FizzBuzz
+
+var fizzBuzz = function(){
+    var outputTarget = document.querySelector( "#fizz-buzz");
+    var outputHtml = "<ul>";
+    var stop = 50;
+    var i = 1;
+
+        while( i < stop ){
+            if( i % 3 === 0 && i % 5 === 0){
+                outputHtml += "<li>FizzBuzz</li>";
+            }
+
+            else if( i % 5 === 0){
+                outputHtml += "<li>Buzz</li>";
+            }
+
+            else if( i % 3 === 0){
+                outputHtml += "<li>Fizz</li>";
+            }
+
+            else {
+                outputHtml += "<li>" + i + "</li>";
+            }
+
+            i++;
+        }
+
+        outputHtml += "</ul>"
+        outputTarget.innerHTML = outputHtml;
+
 };
 
-var compare = function ( userChoice, computerChoice ) {
-    // if ( userChoice === computerChoice ) {
-    //     console.log( "tie" );
-    // }
-    if (userChoice === "rock" && computerChoice === "scissor") {
-        console.log ("ROCK WINS!");
+fizzBuzz();
+
+
+//EvenOdd
+
+var evenOdd = function () {
+
+    var outputTarget = document.querySelector("#even-odd");
+    var outputHtml = "";
+    var i = 15;
+
+    while( i > 0) {
+        if( i % 2 === 0 ){
+            outputHtml += "<li>" + i + " is EVEN</li>"
+        }
+            else {
+                outputHtml += "<li>" + i +" is ODD</li>"
+            }
+
+        i--;
     }
-    else if (userChoice === "scissor" && computerChoice === "paper") {
-        console.log ("SCISSOR WINS!")
-    }
-    else if (userChoice === "paper"  && computerChoice === "rock") {
-        console.log ("PAPER WINS!")
-    }
+
+    outputHtml += "</ul>";
+    outputTarget.innerHTML = outputHtml;
 };
 
-compare ();
-
-
-// var object = function( numberOne ){
-//     var outputTarget = document.querySelector( "#rock-paper-scissor");
-//     var outputHtml = "";
-//
-//         if( numberOne <= 0.3 ) {
-//             outputHtml += "<p>Rock</p>"
-//         }
-//         else if (numberOne <= 0.6) {
-//             outputHtml += "<p>Scissor</p>"
-//         }
-//         else {
-//             outputHtml += "<p>Paper</p>"
-//         }
-//
-//     outputTarget.innerHTML = outputHtml;
-// };
-//
-// object( 0.23 );
-
-
-
-
-// var evenOdd = function() {
-//     var outputTarget = document.querySelector("#even-odd");
-//     var outputHtml = "";
-//     var i = 15;
-//
-//     while( i > 0) {
-//         if( i % 2 === 0 ){
-//             outputHtml += "<li>" + i + " is EVEN</li>"
-//         }
-//             else {
-//                 outputHtml += "<li>" + i +" is ODD</li>"
-//             }
-//
-//         i--;
-//     }
-//
-//     outputHtml += "</ul>";
-//
-//     outputTarget.innerHTML = outputHtml;
-// };
-
-
-// var fizzBuzz = function(){
-//     var outputTarget = document.querySelector( "#fizz-buzz");
-//     var outputHtml = "<ul>";
-//     var stop = 100;
-//     var i = 1;
-//
-//         while( i < stop ){
-//             if( i % 3 === 0 && i % 5 === 0){
-//                 outputHtml += "<li>FizzBuzz</li>";
-//             }
-//
-//             else if( i % 5 === 0){
-//                 outputHtml += "<li>Buzz</li>";
-//             }
-//
-//             else if( i % 3 === 0){
-//                 outputHtml += "<li>Fizz</li>";
-//             }
-//
-//             else {
-//                 outputHtml += "<li>" + i + "</li>";
-//             }
-//
-//             i++;
-//         }
-//
-//         outputHtml += "</ul>"
-//         outputTarget.innerHTML = outputHtml;
-//
-// };
-//
-// evenOdd();
-// fizzBuzz();
-
-
-// var outputTarget = document.querySelector("#even-odd");
-// var outputHtml = "";
-// var i = 15;
-//
-// while( i > 0) {
-//     if( i % 2 === 0 ){
-//         outputHtml += "<li>" + i + " is EVEN</li>"
-//     }
-//         else {
-//             outputHtml += "<li>" + i +" is ODD</li>"
-//         }
-//
-//     i--;
-// }
-//
-// outputHtml += "</ul>";
-//
-// outputTarget.innerHTML = outputHtml;
+evenOdd();
