6de71e0488e39edd9f54c5b2f4951f6647bb43da
Amy Scott <amyscott@Macintosh.local>
2016-09-27 18:19:10 -0500

    Setting up SavvyCoders Hackathon 2

--- AmyKempen/projects/choose-your-own-adventure/site.js
+++ AmyKempen/projects/choose-your-own-adventure/site.js
@@ -1,162 +1,76 @@
 
-//Story with innerHTML that still doesn't work
-    var userOutputTarget = document.querySelector("#cyoa");
-    var storyOutcomeHtml = "";
-    var mainCharacter = {}
-    var ancilliaryCharacter = {}
+//Characters
+var mainCharacter = {}
+var ancilliaryCharacter = {}
 
-    //Data
-    var story = {
-            country: {
-                good: "You pick up a copy of Rick Steves and some good walking shoes.",
-                bad: "It's beach time so sit back and chillax with a margarita. You deserve it. The end."
-            },
-            city: {
-                good: "You grab a cab and head to the Uffizi.",
-                bad: "You realize Rome is in the middle of a zombie apocolypse so you run for dear life in search of other uninfected people."
-            },
-            adventure: {
-                good: "You meet two Italians, fall madly in love, move to Florence, and live happily-ever-after.",
-                bad: "You nerd out on art all afternoon, pig out on pizza, get drunk off wine, and end the night with gelato."
-            }
-    };
-    //Characters
-    var characters = function () {
-        mainCharacter.name = prompt ( "What's your name?" );
-        ancilliaryCharacter.name = prompt ( "Who do you want to travel with?" );
+//Data
+var story = {
+        country: {
+            good: "You pick up a copy of Rick Steves and some good walking shoes.",
+            bad: "It's beach time so sit back and chillax with a margarita. You deserve it. The end."
+        },
+        city: {
+            good: "You grab a cab and head to the Uffizi.",
+            bad: "You realize Rome is in the middle of a zombie apocolypse so you run for dear life in search of other uninfected people."
+        },
+        adventure: {
+            good: "You meet two Italians, fall madly in love, move to Florence, and live happily-ever-after.",
+            bad: "You nerd out on art all afternoon, pig out on pizza, get drunk off wine, and end the night with gelato."
+        }
+};
+
+// added here to keep things organized (following "vars-on-top" rule)
+var promptText;
+
+//Helper functions
+var updateOutput = function ( input ) {
+    document.querySelector( "#output" ).textContent = input;
+};
+
+
+//Main CYOA
+
+//Step 1: Names
+mainCharacter.name = prompt ( "What's your name?" );
+
+ancilliaryCharacter.name = prompt ( "Who do you want to travel with?" );
+
+promptText = prompt ( mainCharacter.name + " and " + ancilliaryCharacter.name + " decide to go on a trip together. Should you go to Italy or Mexico"); // promptText is "Mexico"
+
+
+//Step 2: Country
+if ( promptText === "Italy" ) { // promptText is "Mexico", condition is false, code from lines 45-50 will not run
+    promptText = prompt ( story.country.good + " Which city would you like to visit, Florence or Rome?" );
+    while ( promptText !== "Italy" && promptText !== "Mexico" ) {
+        promptText = prompt ( mainCharacter.name + " and " + ancilliaryCharacter.name + " decide to go on a trip together. Should you go to Italy or Mexico");
     }
+}
+else if ( promptText === "Mexico" ) { // promptText is "Mexico", condition is true, code from lines 51-53 will run
+    updateOutput ( story.country.bad ); // promptText is "Mexico", "bad" condition is appended as HTML to screen
+}
 
-    characters ();
-    prompt ( mainCharacter.name + " and " + ancilliaryCharacter.name + " decide to go on a trip together. Should you go to Italy or Mexico");
 
-    //Country
-    var countries = function (){
-        if ( characterPromptResult = "Italy" ) {
-            prompt ( story.country.good + " Which city would you like to visit, Florence or Rome?" );
-            while ( characterPromptResult != "Italy" && characterPromptResult != "Mexico" ) {
-                prompt ( mainCharacter.name + " and " + ancilliaryCharacter.name + " decide to go on a trip together. Should you go to Italy or Mexico");
-            }
-        }
-        else if ( characterPromptResult = "Mexico" ) {
-            storyOutcomeHtml = "<p>" + story.country.bad + "</p>";
-        }
-    };
-    countries ();
-    userOutputTarget.innerHTML = storyOutcomeHtml;
-
-//City
-    var city = function (){
-        if ( cityPromptResult = "Florence" ) {
-            prompt ( story.city.good + " Are you feeling adventurous, yes or no?" );
-            while ( cityPromptResult != "Florence" && cityPromptResult != "Rome" ) {
-                prompt ( story.country.good + " Which city would you like to visit, Florence or Rome?" );
-            }
-        }
-        else if ( cityPromptResult = "Rome" ) {
-            storyOutcomeHtml = "<p>" + story.city.bad + "</p>";
-        }
-    };
-    city ();
-    userOutputTarget.innerHTML = storyOutcomeHtml;
-
+//Step 3: City
+while ( promptText !== "Florence" && promptText !== "Rome" ) { // promptText is "Mexico", condition is true, code from lines 57-59 will run
+    promptText = prompt ( story.country.good + " Which city would you like to visit, Florence or Rome?" ); // promptText is now "Rome"
+}
+if ( promptText === "Florence" ) { // promptText is "Rome", condition is false, code from lines 60-62 will not run
+    promptText = prompt ( story.city.good + " Are you feeling adventurous, yes or no?" );
+}
+else if ( promptText === "Rome" ) { // promptText is "Rome", condition is true, code from lines 63-65 will not run
+    updateOutput ( story.city.bad ); // promptText is "Rome", "bad" condition is appended as HTML to screen
+};
 
 //Step 4: Adventure
-    var adventure = function (){
-        if ( adventurePromptResult === "yes" ) {
-            updateOutput ( story.adventure.good );
-            while ( adventurePromptResult !== "yes" && adventurePromptResult !== "no" ) {
-                prompt ( story.city.good + " Are you feeling adventurous, yes or no?" );
-            }
-        }
-        else if ( adventurePromptResult === "no" ) {
-            storyOutcomeHtml = "<p>" + story.adventure.bad + "</p>";
-        }
-    };
-    adventure ();
-    userOutputTarget.innerHTML = storyOutcomeHtml;
-
-
-
-
-//Story with lots of functions that doesn't work
-// //Characters
-// var mainCharacter = {}
-// var ancilliaryCharacter = {}
-//
-// //Data
-// var story = {
-//         country: {
-//             good: "You pick up a copy of Rick Steves and some good walking shoes.",
-//             bad: "It's beach time so sit back and chillax with a margarita. You deserve it. The end."
-//         },
-//         city: {
-//             good: "You grab a cab and head to the Uffizi.",
-//             bad: "You realize Rome is in the middle of a zombie apocolypse so you run for dear life in search of other uninfected people."
-//         },
-//         adventure: {
-//             good: "You meet two Italians, fall madly in love, move to Florence, and live happily-ever-after.",
-//             bad: "You nerd out on art all afternoon, pig out on pizza, get drunk off wine, and end the night with gelato."
-//         }
-// };
-//
-// //Helper functions
-// var updateOutput = function ( input ) {
-//     document.querySelector( "#output" ).textContent = input;
-// };
-//
-//
-// //Main CYOA
-//
-// //Step 1: Names
-// var characters = function () {
-//     mainCharacter.name = prompt ( "What's your name?" );
-//     ancilliaryCharacter.name = prompt ( "Who do you want to travel with?" );
-// }
-//
-// characters ();
-// characterPromptResult = prompt ( mainCharacter.name + " and " + ancilliaryCharacter.name + " decide to go on a trip together. Should you go to Italy or Mexico");
-//
-//
-// //Step 2: Country
-// if ( characterPromptResult = "Italy" ) {
-//     prompt ( story.country.good + " Which city would you like to visit, Florence or Rome?" );
-//     while ( characterPromptResult != "Italy" && characterPromptResult != "Mexico" ) {
-//         prompt ( mainCharacter.name + " and " + ancilliaryCharacter.name + " decide to go on a trip together. Should you go to Italy or Mexico");
-//     }
-// }
-// else if ( characterPromptResult = "Mexico" ) {
-//     updateOutput ( story.country.bad );
-// }
-//
-//
-// //Step 3: City
-// var city = function (){
-//     if ( cityPromptResult === "Florence" ) {
-//         cityPromptResult = prompt ( story.city.good + " Are you feeling adventurous, yes or no?" );
-//         while ( cityPromptResult !== "Florence" && cityPromptResult !== "Rome" ) {
-//             cityPromptResult = prompt ( story.country.good + " Which city would you like to visit, Florence or Rome?" );
-//         }
-//     }
-//     else if ( cityPromptResult === "Rome" ) {
-//         updateOutput ( story.city.bad );
-//     }
-// };
-// city ();
-//
-//
-// //Step 4: Adventure
-// var adventure = function (){
-//     if ( cityPromptResult === "yes" ) {
-//         updateOutput ( story.adventure.good );
-//         while ( cityPromptResult !== "yes" && cityPromptResult !== "no" ) {
-//             promptText = prompt ( story.city.good + " Are you feeling adventurous, yes or no?" );
-//         }
-//     }
-//     else if ( cityPromptResult === "no" ) {
-//         updateOutput ( story.adventure.bad );
-//     }
-// };
-// adventure ();
+while ( promptText !== "yes" && promptText !== "no" ) { // promptText is "Rome", condition is true, code from lines 68-70 will run
+    promptText = prompt ( story.city.good + " Are you feeling adventurous, yes or no?" ); // promptText is now "no"
+}
+if ( promptText === "yes" ) { // promptText is "no", condition is false, code will not run
+    updateOutput ( story.adventure.good );
+}
+else if ( promptText === "no" ) { // promptText is "no", condition is true, code from lines 74-76 will run
+    updateOutput ( story.adventure.bad );
+};
 
 
 
