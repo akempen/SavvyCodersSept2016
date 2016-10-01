d1c95c4b0624fab6eda80d9f98ac8d24177ba843
Amy Scott <amyscott@macintosh.attlocal.net>
2016-10-01 06:59:56 -0500

    Update hamburger menu javascript

--- AmyKempen/theme/site.js
+++ AmyKempen/theme/site.js
@@ -1,7 +1,35 @@
-$(".hamburger").click (function () { // hamburger is visible
-    $(".dropdown").slideToggle(); // slideToggle triggered, hamburger hidden
-});
+// $(".hamburger").click (function () { // hamburger is visible
+//     $(".dropdown").slideToggle(); // slideToggle triggered, hamburger hidden
+// });
 
+$(".hamburger").click(
+    function() {        
+        if ($( ".dropdown" ).hasClass( "toggle" )){
+            $( ".dropdown" ).removeClass("toggle")
+        }
+        else {
+            $( ".dropdown" ).addClass("toggle")
+        }
+    }
+);
+
+$("li").on( "mouseover", function(){
+    $( this ).addClass( "highlight");
+} );
+
+$("li").on( "mouseleave", function(){
+    $( this ).removeClass( "highlight");
+} );
+
+
+
+// $("li").on( "mouseover", function(){
+//     $( this ).addClass( "highlight");
+// } );
+//
+// $("li").on( "mouseleave", function(){
+//     $( this ).removeClass( "highlight");
+// } );
 
 
 
